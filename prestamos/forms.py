dc96478f084da38291028ae92bb5cc2a3b5c0676
tomy07417 <tamundarain@fi.uba.ar>
2022-08-16 17:11:02 -0300

    sprint7

--- prestamos/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Prestamos
-
-
-class PrestamosForm(forms.ModelForm):
-    class Meta:
-        model = Prestamos
-        fields = ['loan_type','loan_date','loan_total','customer_id']
